8a9121d82e0253fa690a097ddf5ea09d98a9436a
Nick Fedorenko <nfm12121994@gmail.com>
2025-09-29 17:25:46 +0300

    Delete UI_homework directory

--- UI_homework/src/backend/services/shop/routes.py
+++
@@ -1,9 +0,0 @@
-from enum import StrEnum
-
-class Route(StrEnum):
-    REGISTER = "/auth/register"
-    LOGIN = "/auth/login"
-    GET_CART = "/cart/"
-    ADD_ITEM = "/cart/add"
-    REMOVE_ITEM = "/cart/remove"
-    GET_CATALOG = "/catalog/"
--- UI_homework/src/utils/helpers.py
+++
@@ -1,6 +0,0 @@
-MAX_BODY_SIZE = 150  # Максимальный размер тела в байтах
-
-def truncate_body(body: str, max_size: int = MAX_BODY_SIZE) -> str:
-    if len(body.encode("utf-8")) > max_size:
-        body = "<TRUNCATED> " + body.encode("utf-8")[:max_size].decode("utf-8", "ignore")
-    return body
